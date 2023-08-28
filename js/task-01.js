7070883457839d4545330a9c451a73775190bc18
Mart <marta.nikiel@yahoo.com>
2023-08-28 10:37:05 +0200

    added first task

---
+++ js/task-01.js
@@ -0,0 +1,10 @@
+const listCategories = document.querySelectorAll(".item");
+console.log(`Number of categories: ${listCategories.length}`);
+
+listCategories.forEach((category) => {
+  const title = category.querySelector("h2");
+  const categoryName = title.textContent;
+  const elementsNumber = category.querySelectorAll("li").length;
+
+  console.log(`Category: ${categoryName} Elements: ${elementsNumber}`);
+});
