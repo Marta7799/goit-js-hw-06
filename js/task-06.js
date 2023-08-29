1d2c611c67ed93266f555baa9aecf4fdf573796b
Mart <marta.nikiel@yahoo.com>
2023-08-29 13:32:18 +0200

    added sixth task

---
+++ js/task-06.js
@@ -0,0 +1,33 @@
+// const textInput = document.querySelector("#validation-input");
+
+// console.log(textInput.getAttribute("data-length"));
+
+// textInput.addEventListener("blur", (event) => {
+//   if (event.target.value.length == textInput.getAttribute("data-length")) {
+//     textInput.classList.add("valid");
+//     if (textInput.classList.contains("invalid")) {
+//       textInput.classList.remove("invalid");
+//     }
+//   } else {
+//     if (textInput.classList.contains("valid")) {
+//       textInput.classList.remove("valid");
+//     }
+//     textInput.classList.add("invalid");
+//   }
+// });
+
+const textInput = document.getElementById("validation-input");
+
+const length = textInput.getAttribute("data-length");
+
+const validation = () => {
+  if (textInput.value.length < length) {
+    textInput.classList.add("invalid");
+    textInput.classList.remove("valid");
+  } else {
+    textInput.classList.add("valid");
+    textInput.classList.remove("invalid");
+  }
+};
+
+textInput.addEventListener("blur", validation);
